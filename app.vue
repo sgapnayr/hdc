31fc9c8e226c34139b567199f1c5a10bf46a9bb6
Ryan Paglione <ryanpaglionemusic@gmail.com>
2023-07-13 11:45:30 -0500

    Help Modal Mobile

--- app.vue
+++ app.vue
@@ -4,6 +4,21 @@
   </NuxtLayout>
 </template>
 
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        {
+          src: '//code.tidio.co/3vxi4rj20xvvbortj5b7alykul8wvkk7.js',
+          async: true,
+        },
+      ],
+    }
+  },
+}
+</script>
+
 <style>
 h1 {
   color: #403e48;
